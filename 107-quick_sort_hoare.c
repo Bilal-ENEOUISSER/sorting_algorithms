c7017df00bfc909c687fe53c23b0de4672ae4b41
Bilal ENEOUISSER <77547073+Bilal-ENEOUISSER@users.noreply.github.com>
2024-02-20 00:15:50 +0100

    Update 107-quick_sort_hoare.c

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,6 +1,6 @@
 #include "sort.h"
 
-void switch_ints(int *a, int *b);
+void switch_ints(int *j, int *k);
 int hoare_partition(int *array, size_t size, int left, int right);
 void hoare_sort(int *array, size_t size, int left, int right);
 void quick_sort_hoare(int *array, size_t size);
