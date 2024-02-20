db19de7790df8322a33f300630898638c5841572
Bilal-ENEOUISSER <77547073+Bilal-ENEOUISSER@users.noreply.github.com>
2024-02-20 01:11:02 +0100

    edit

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,53 +3,53 @@
 void swap_list(listint_t *, listint_t *, listint_t **);
 
 /**
-* insertion_sort_list - sorts a list using insertion sort
-*
-* @list: double pointer to head of list
-*/
+ * insertion_sort_list - sorts a list using insertion sort
+ *
+ * @list: double pointer to head of lis
+ */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *cur = NULL;
-	listint_t *prv = NULL;
+	listint_t *curr = NULL;
+	listint_t *prev = NULL;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	cur = (*list)->next;
-	prv = cur->prv;
-	while (cur != NULL)
+	curr = (*list)->next;
+	prev = curr->prev;
+	while (curr != NULL)
 	{
-		prv = cur->prv;
-		while (prv != NULL && prv->n > cur->n)
+		prev = curr->prev;
+		while (prev != NULL && prev->n > curr->n)
 		{
-			swap_list(cur, prv, list);
-			prv = cur->prv;
+			swap_list(curr, prev, list);
+			prev = curr->prev;
 		}
-		cur = cur->next;
+		curr = curr->next;
 	}
 }
 
 /**
-* swap_list - swaps two members of a list
-*
-* @cur: current node
-* @prv: previous node
-* @head: head of list
-*/
-void swap_list(listint_t *cur, listint_t *prv, listint_t **head)
+ * swap_list - swaps two members of a list
+ *
+ * @curr: current node
+ * @prev: previous node
+ * @head: head of list
+ */
+void swap_list(listint_t *curr, listint_t *prev, listint_t **head)
 {
-	listint_t *temp1 = cur->next;
-	listint_t *temp2 = prv->prv;
+	listint_t *temp1 = curr->next;
+	listint_t *temp2 = prev->prev;
 
 	if (temp1 != NULL)
-		temp1->prv = prv;
+		temp1->prev = prev;
 	if (temp2 != NULL)
-		temp2->next = cur;
-	cur->prv = temp2;
-	prv->next = temp1;
-	cur->next = prv;
-	prv->prv = cur;
-	if (*head == prv)
-		*head = cur;
+		temp2->next = curr;
+	curr->prev = temp2;
+	prev->next = temp1;
+	curr->next = prev;
+	prev->prev = curr;
+	if (*head == prev)
+		*head = curr;
 	print_list(*head);
 }
